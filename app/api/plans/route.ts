b094acc4c8909914ead785568cd40f511ea2a8ab
Jorge Sardon <anibal317@gmail.com>
2024-12-17 10:00:24 -0300

    enabling options cors

--- app/api/plans/route.ts
+++ app/api/plans/route.ts
@@ -1,21 +1,40 @@
 // app/api/plans/route.ts
-
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: Request) {
+// Configuración de CORS
+const corsHeaders = {
+  'Access-Control-Allow-Origin': 'https://facturacionale.netlify.app', // Reemplaza con el dominio permitido
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
 
+// Manejo de solicitudes OPTIONS (preflight)
+export async function OPTIONS() {
+  return NextResponse.json(null, {
+    status: 204, // No Content
+    headers: corsHeaders,
+  });
+}
+
+// Manejo de solicitudes GET
+export async function GET(req: Request) {
   try {
     const plans = await prisma.plan.findMany({
       include: {
-        planfeature: true
-      }
-    }
-    ); // Suponiendo que tienes un modelo "Plan"
-    return NextResponse.json({ plans });
+        planfeature: true,
+      },
+    });
+    return NextResponse.json(plans, {
+      status: 200,
+      headers: corsHeaders,
+    });
   } catch (error) {
-    return NextResponse.json({ error: 'Error al obtener los planes' }, { status: 500 });
+    return NextResponse.json({ error: 'Error fetching benefits' }, {
+      status: 500,
+      headers: corsHeaders,
+    });
   }
 }
