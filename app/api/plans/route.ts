62674842c740679b41d88d44c8e372da47e5bed8
Jorge Sardon <anibal317@gmail.com>
2024-12-18 16:35:57 -0300

    fixing feature endpoint

--- app/api/plans/route.ts
+++ app/api/plans/route.ts
@@ -4,21 +4,6 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// Configuración de CORS
-const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://facturacionale.netlify.app', // Reemplaza con el dominio permitido
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
-
-// Manejo de solicitudes OPTIONS (preflight)
-export async function OPTIONS() {
-  return NextResponse.json(null, {
-    status: 204, // No Content
-    headers: corsHeaders,
-  });
-}
-
 // Manejo de solicitudes GET
 export async function GET(req: Request) {
   try {
@@ -28,13 +13,11 @@ export async function GET(req: Request) {
       },
     });
     return NextResponse.json(plans, {
-      status: 200,
-      headers: corsHeaders,
+      status: 200
     });
   } catch (error) {
-    return NextResponse.json({ error: 'Error fetching benefits' }, {
-      status: 500,
-      headers: corsHeaders,
+    return NextResponse.json({ error: 'Error fetching Plans' }, {
+      status: 500
     });
   }
 }
