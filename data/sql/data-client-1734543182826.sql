d7036b0cb1a449132a9792388fba4d70dee1d008
Jorge Sardon <anibal317@gmail.com>
2024-12-18 14:33:15 -0300

    addgin sqlInser for client

---
+++ data/sql/data-client-1734543182826.sql
@@ -0,0 +1,8 @@
+INSERT INTO test.client (name,img,link,active,`ordering`) VALUES
+	 ('Arca','/imgs/clients/arca.jpeg','https://www.arca.com',1,1),
+	 ('Distripag','/imgs/clients/distripag.png','https://www.distripag.com',1,2),
+	 ('Globant','/imgs/clients/globant.png','https://www.globant.com',1,3),
+	 ('STD','/imgs/clients/std.png','https://www.std.com',1,4),
+	 ('Teracode','/imgs/clients/teracode.jpeg','https://www.teracode.com',1,5),
+	 ('Thor','/imgs/clients/thor.jpg','https://www.thor.com',1,6),
+	 ('Coto','/imgs/clients/coto.jpeg','https://www.coto.com.ar/',1,0);
